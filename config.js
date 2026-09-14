2e20f1ccaa750e4bb5bdf00c0896f267b3ca788b
Colin Kristianti <colinkristianti@CK.local>
2026-09-14 16:48:23 +0800

    Make Apps Script the default backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TOCFL_CONFIG={
-  backend:'supabase',
+  backend:'apps-script',
   appsScriptUrl:'https://script.google.com/macros/s/AKfycbw4iEYalo_4r9ENJCHgJ3OpH9cmGXQVpeAGRF4jyfQssbVtyUtNCDNgtF2ZVTnoJjwj/exec',
   supabaseUrl:'https://zhtphxloujxuczxesjez.supabase.co',
   supabasePublishableKey:'sb_publishable_Iamki_96Ca1zt99FxKXaYQ_rXp_qDBF'
