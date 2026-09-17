b1fb730f3487f0598df70d3693c8aafa9ce3d937
Colin Kristianti <colinkristianti@CK.local>
2026-09-17 11:29:09 +0800

    Use new Apps Script deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.TOCFL_CONFIG={
   backend:'apps-script',
-  appsScriptUrl:'https://script.google.com/macros/s/AKfycbw4iEYalo_4r9ENJCHgJ3OpH9cmGXQVpeAGRF4jyfQssbVtyUtNCDNgtF2ZVTnoJjwj/exec',
+  appsScriptUrl:'https://script.google.com/macros/s/AKfycbzi_EHuY_a8qGVvCyDrPT2MYiRvrFtED_dYBtMWCSxGNgl6wFYdXyKqqBO22R7xPhHf7A/exec',
   supabaseUrl:'https://zhtphxloujxuczxesjez.supabase.co',
   supabasePublishableKey:'sb_publishable_Iamki_96Ca1zt99FxKXaYQ_rXp_qDBF'
 };
